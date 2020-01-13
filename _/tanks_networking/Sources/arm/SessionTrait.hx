69e8f362767de42d7c06fb9fef36b6c3de42bbeb
Sandy <48373918+Sandy10000@users.noreply.github.com>
2020-01-13 21:43:29 +0900

    network -> netsync

--- _/tanks_networking/Sources/arm/SessionTrait.hx
+++ _/tanks_networking/Sources/arm/SessionTrait.hx
@@ -2,12 +2,12 @@ package arm;
 
 class SessionTrait extends iron.Trait {
 
-    public static var session:kha.network.Session = null;
+    public static var session:kha.netsync.Session = null;
 
     public function new() {
         super();
 
-        session = new kha.network.Session(2, "localhost", 6789);
+        session = new kha.netsync.Session(2, "localhost", 6789);
         session.waitForStart(startSession, onSessionRefused, onSessionError, onSessionClosed, resetSession);
     }
 
